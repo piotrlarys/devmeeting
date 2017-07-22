dd61f4e6dfe916a6d3ca28e47b0d1537b4cf9509
Piot Larys <piotr.larys@nsp.biz.pl>
2017-07-22 10:50:29 +0200

    tak1

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,18 @@
+import { Product } from './product.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  product: Product;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
