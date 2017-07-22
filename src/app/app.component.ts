22c7e6fc45f875b975d576b1182077a375e4dd89
Piot Larys <piotr.larys@nsp.biz.pl>
2017-07-22 15:31:05 +0200

    fixed names etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,23 +12,18 @@ export class AppComponent implements OnInit {
   title = 'app';
   myInput = new FormControl();
   products: Product[];
-  filteredProducts: Product[];
-  filteredPromotedProducts: Product[];
   promotedProducts: Product[];
 
   constructor(private productRepository: ProductRepositoryService) {
   }
 
   ngOnInit(): void {
-    this.filteredProducts = this.productRepository.getProducts();
     this.products = this.productRepository.getProducts();
-    this.filteredPromotedProducts = this.productRepository.getPromotedProducts();
     this.promotedProducts = this.productRepository.getPromotedProducts();
 
     this.myInput.valueChanges.subscribe(value => {
-      this.filteredProducts = this.productRepository.filterProducts(value);
-      this.filteredPromotedProducts = this.productRepository.filterPromotedProducts(value);
+      this.products = this.productRepository.filterProducts(value);
+      this.promotedProducts = this.productRepository.filterPromotedProducts(value);
     });
   }
-
 }
